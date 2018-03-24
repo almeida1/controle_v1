75c4c89378e319d1e52a2dec3733711a08bd1363
Lab103 <Lab103@LAB101-21>
2018-03-24 18:05:59 -0300

    Versao inicial

---
+++ ControleDeEmprestimo/teste/br/emprestimo/testeUnitario/UC01RegistrarEmprestimoDeLivro.java
@@ -0,0 +1,63 @@
+package br.emprestimo.testeUnitario;
+
+import static org.junit.Assert.*;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import br.emprestimo.modelo.Emprestimo;
+import br.emprestimo.modelo.Livro;
+import br.emprestimo.modelo.Usuario;
+import br.emprestimo.servico.ServicoEmprestimo;
+
+public class UC01RegistrarEmprestimoDeLivro {
+	static private Livro livro;
+	static private Usuario usuario;
+	static private ServicoEmprestimo servico;
+	static private Emprestimo emprestimo;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		// cenario
+		livro = new Livro();
+		livro.setIsbn("121212");
+		livro.setTitulo("Engenharia de Software");
+		livro.setAutor("Pressman");
+		usuario = new Usuario();
+		usuario.setRa("11111");
+		usuario.setNome("Jose da Silva");
+		servico = new ServicoEmprestimo();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/*
+	 * Verifica o comportamento da classe servico metodo empresta
+	 */
+	@Test
+	public void CT01UC01FB_registra_emprestimo_com_sucesso() {
+		assertNotNull(servico.empresta(livro, usuario));
+	}
+
+	/*
+	 * Verifica o comportamento da classe servico metodo empresta
+	 */
+	@Test(expected = RuntimeException.class)
+	public void CT02UC01FB_registrar_emprestimo_com_dados_invalidos() {
+		assertNull(servico.empresta(null, usuario));
+	}
+	/*
+	 * Verifica o comportamento da classe servico metodo empresta
+	 */
+	@Test
+	public void CT03UC01FB_registrar_emprestimo_com_dados_invalidos() {
+		try {
+			servico.empresta(null, usuario);
+			fail("deveria lançar uma exceção");
+		} catch (RuntimeException e) {
+			assertEquals("Dados inválidos.", e.getMessage());
+		}
+	}
+}
